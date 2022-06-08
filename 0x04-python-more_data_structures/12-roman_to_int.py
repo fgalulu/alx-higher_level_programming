b00e2e7acfaad84e55a49c556ac8c8eccd7378b7
fgalulu <fionasonny132@gmail.com>
2022-06-08 14:21:03 -0700

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         i = 0
         value = 0
-        for i in roman_string:
+        for i in range(len(roman_string)):
             if i > 0 and roman_values[roman_string[i]] > roman_values[roman_string[i - 1]]:
                 value += roman_values[roman_string[i]] - 2 * roman_values[roman_string[i - 1]]
             else:
