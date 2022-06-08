fe257486af1ef5b6861f3d9bc4d5ec8b422183f5
fgalulu <fionasonny132@gmail.com>
2022-06-08 14:09:11 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str:
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        i = 0
+        value = 0
+        for i in roman_string:
+            if i > 0 and roman_values[roman_string[i]] > roman_values[roman_string[i - 1]]:
+                value += roman_values[roman_string[i]] - 2 * roman_values[roman_string[i - 1]]
+            else:
+                value += roman_values[roman_string[i]]
+        return value
+    else:
+        return(0)
