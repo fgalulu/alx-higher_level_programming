8a2b43d69b57358ea702b31a5b55871fc30fe0a5
fgalulu <fionasonny132@gmail.com>
2022-08-30 05:49:44 -0700

    classess

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -76,5 +76,5 @@ class Rectangle:
         for n in range(0, self.__height):
             rect += "#" * self.__width
             if n != self.__height - 1:
-                rect += '/n'
+                rect += '\n'
         return rect
