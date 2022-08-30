f7e183922bf6051f2936d0f7a5477024e9fde19a
fgalulu <fionasonny132@gmail.com>
2022-08-30 06:57:18 -0700

    fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -98,7 +98,7 @@ class Rectangle:
         type(self).number_of_instances -= 1
 
     @staticmethod
-    def bigger_or_equal(rect1, rect2):
+    def bigger_or_equal(rect_1, rect_2):
         """
             return biggest rectangle based on area.
         """
