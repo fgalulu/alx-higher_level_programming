767c9b74fed221722d662a79f08677d84c954d7a
fgalulu <fionasonny132@gmail.com>
2022-08-29 23:54:46 -0700

    task 1 fix 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 """
-    A class Rectanglethat defines a rectangle.
+    A class Rectangle that defines a rectangle with width and height.
 """
 
 
 class Rectangle:
     """
         define a rectangle with width and height.
+
+        Attributes:
+            width(int): rectangle width.
+            height(int): rectangle height.
     """
     def __init__(self, width=0, height=0):
+        """
+            Intitialize a new Rectangle instance.
+
+            Args:
+                width(int): rectangle width.
+                height(int): rectangle height.
+        """
         self.width = width
         self.height = height
 
