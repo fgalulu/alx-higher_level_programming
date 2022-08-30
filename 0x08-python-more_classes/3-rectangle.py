d00335d683d1fa06bf2faa4400ed0fcad712fbd2
fgalulu <fionasonny132@gmail.com>
2022-08-30 05:33:34 -0700

    task 3 fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -68,7 +68,7 @@ class Rectangle:
             return 0
         return 2 * (self.__width + self.__height)
 
-    def __str__(sekf):
+    def __str__(self):
         """return string representation."""
         rect = []
         if self.__height == 0 or self.__width == 0:
