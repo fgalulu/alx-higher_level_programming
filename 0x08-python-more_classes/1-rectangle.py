c9652fcda4ffe1278c3131df342240394ded8cea
fgalulu <fionasonny132@gmail.com>
2022-08-30 00:26:51 -0700

    fix height print error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
         """
             Get/set the width of the rectangle
         """
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
