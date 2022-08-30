9b4cdb89dfae29f7f9f6b6a9c46700b1324cd497
fgalulu <fionasonny132@gmail.com>
2022-08-30 00:15:19 -0700

    fix module documeaed error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    A class Rectangle that defines a rectangle with width and height.
-"""
+"""Defines a rectangle with width and height."""
 
 
 class Rectangle:
@@ -23,14 +21,32 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def width(self, value*):
-        if value:
-            self.width = value
-        else:
-            return self.width
+    @property
+    def width(self):
+        """
+            Get/set the width of the rectangle.
+        """
+        return self.__width
 
-    def height(self, value*):
-        if value:
-            self.height = value
-        else:
-            return self.height
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer.")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            Get/set the width of the rectangle
+        """
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer.")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
