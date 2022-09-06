1d9d6bd799446757f894ed858c0c468bcfcd3f16
fgalulu <fionasonny132@gmail.com>
2022-09-06 16:04:26 +0000

    semisstarndard fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i=0; i<myVar.length; i++){
-	console.log(myVar[i])
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
 }
