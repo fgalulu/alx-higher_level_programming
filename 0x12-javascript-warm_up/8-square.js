9c7f9551c154f0da562c08d42141bc016f2c6109
fgalulu <fionasonny132@gmail.com>
2022-09-06 16:29:30 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let myVar = parseInt(process.argv[2]);
+
+if (isNaN(myVar) || process.argv[2] === undefined){
+	console.log('Missing size');
+}
+let myString = 'X';
+for (let i=1; i<myVar; i++){
+	myString += 'X';
+}
+while (myVar > 0){
+	console.log(myString);
+	myVar--;
+}
