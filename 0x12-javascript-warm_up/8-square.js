e8bf0cce43ad1feeb6544dcd7cd21d3ddb3bf9dc
fgalulu <fionasonny132@gmail.com>
2022-09-06 16:31:09 +0000

    semistandard fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,14 @@
 
 let myVar = parseInt(process.argv[2]);
 
-if (isNaN(myVar) || process.argv[2] === undefined){
-	console.log('Missing size');
+if (isNaN(myVar) || process.argv[2] === undefined) {
+  console.log('Missing size');
 }
 let myString = 'X';
-for (let i=1; i<myVar; i++){
-	myString += 'X';
+for (let i = 1; i < myVar; i++) {
+  myString += 'X';
 }
-while (myVar > 0){
-	console.log(myString);
-	myVar--;
+while (myVar > 0) {
+  console.log(myString);
+  myVar--;
 }
