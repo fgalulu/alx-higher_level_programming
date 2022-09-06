e4c56c7821fcce3ca72ef58864f5218b349094ce
fgalulu <fionasonny132@gmail.com>
2022-09-06 14:21:24 +0000

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const myVar = parseInt(process.argv[2]);
-if (myVar.isInteger()) {
+if (process.argv[2] === undefined || isNaN(myVar)) {
   console.log('My number: ' + myVar);
 } else {
   console.log('Not a number');
