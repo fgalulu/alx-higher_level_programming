bba538dcc3f30f3c91ee77dbe257d95612a0d559
fgalulu <fionasonny132@gmail.com>
2022-09-11 01:28:31 -0700

    collate fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,14 +6,15 @@ ALTER DATABASE
 	COLLATE utf8mb4_unicode_ci;
 
 -- convert table "first_table' to UTF8
+USE hbtn_0c_0;
 ALTER TABLE
-	hbtn_0c_0.first_table
+	first_table
 	CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+        COLLATE utf8mb4_unicode_ci;
 
 -- convert column 'name' to UTF8
 ALTER TABLE
-	hbtn_0c_0.first_table
+	first_table
 	MODIFY name
 	VARCHAR(256)
 	CHARACTER SET utf8mb4
