157a3a5b87a7d8b9433a8c065bfba4b56de2d1c5
fgalulu <fionasonny132@gmail.com>
2022-09-11 13:18:40 +0000

    task 8 -fix 2

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the db,
-if the table is empty print nothing
+list the first State object from a database
 """
 
 import sqlalchemy
@@ -11,12 +10,13 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    first_state = session.query(State).order_by(State.id_first())
+    first_state = session.query(State).order_by(State.id).first()
     if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
