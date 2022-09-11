96428c0e9b60324529cf2940d03ac02c907f8652
fgalulu <fionasonny132@gmail.com>
2022-09-10 23:35:29 -0700

    task 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- list all rows of the table first_table from the db hbtn_0c_0 in MySQL server
+-- all fields should be printed
+-- the database name will be passed as an arguement of the myql command
+
+SELECT * FROM first_table;
