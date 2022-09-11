3e90836da9b2145889143f7266112258a3648738
fgalulu <fionasonny132@gmail.com>
2022-09-10 23:40:11 -0700

    task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- insert a new row to the table first_table in your MySQL server
+-- New row:
+-- id = 89
+-- name = Best School
+-- the db name will be passed as an arguement of the mysql command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton school');
