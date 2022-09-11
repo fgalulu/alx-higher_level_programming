32773b0a0a03aad59151642057f5d0e79ecee75e
fgalulu <fionasonny132@gmail.com>
2022-09-11 10:20:42 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Takes argument and displays all values in the states
+table where name matches the argument.
+Your scripts should take 4 arguements: mysql username, password,
+database name and state name.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         password=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+                   id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
