147ec63653184d17346451c61a40ef593cec40b2
fgalulu <fionasonny132@gmail.com>
2022-09-11 01:00:09 -0700

    task 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- convert hbtn_0c_0 db to utf8 in mysql server.
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utfmb4_unicode_cli;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utfmb4 COLLATE utfmb4_unicode_cli;
