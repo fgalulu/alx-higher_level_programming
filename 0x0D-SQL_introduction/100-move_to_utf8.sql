2b5a5efbba84de7ae451869f3d2e5c9ce0f9e645
fgalulu <fionasonny132@gmail.com>
2022-09-11 01:05:54 -0700

    task 17 - fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,8 +2,8 @@
 -- in MySQL Server
 ALTER DATABASE
 	hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
 
 -- convert table "first_table' to UTF8
 ALTER TABLE
