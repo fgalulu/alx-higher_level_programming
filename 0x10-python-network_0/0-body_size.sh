ea0032b61cd6b9171fd75a40d75fb6e837fcec21
fgalulu <fionasonny132@gmail.com>
2022-09-22 11:02:35 -0700

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script thattakes in a URL, sends a request to that URL and displays
 #the bosy of the response.
 
-curl -so /dev/null "$1" -w '%{size_download}\n'
+curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
