0ad2eba72c751c4dd22ee4959c705f129c20ee6f
fgalulu <fionasonny132@gmail.com>
2022-09-22 18:10:55 +0000

    pycodestyle fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # function that finds a peak in unsorted arrays.
 
+
 def find_peak(list_of_integers):
     """find largest value in unsorted array"""
-
-   if list_of_integers:
-       list_of_integers.sort()
-       return list_of_integers[-1]
-   else:
-       return None
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
