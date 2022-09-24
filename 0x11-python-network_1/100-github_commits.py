177e1b250f50eaf91ba55e3c0e6842192105be22
fgalulu <fionasonny132@gmail.com>
2022-09-24 06:01:47 +0000

    task 4

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,8 +9,13 @@ from sys import argv
 if __name__ == '__main__':
     url = "https://api.github.com/repos/{}/{}commits".format(argv[2], argv[1])
     r = requests.get(url)
-    commits = r.json
+    commits = r.json()
 
-    for coomit in commits[:10]:
-        print(commit.get('sha'), end=": ")
-        print(commit.get('commit').get('author').get('name'))
+    for commit in commits:
+        i = 0
+        while i < 10:
+            print(commit)
+            print(commit.get('sha'), end=": ")
+            print(commit.get('commit').get('author').get('name'))
+            i += 1
+        break
