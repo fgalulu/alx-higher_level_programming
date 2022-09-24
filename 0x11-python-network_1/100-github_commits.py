4292305f300b38b2844136a31e82e3392a2ed3bd
fgalulu <fionasonny132@gmail.com>
2022-09-24 06:12:49 +0000

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     commits = r.json()
 
     for commit in commits[:10]:
-            print(commit.get('sha'), end=": ")
-            print(commit.get('commit').get('author').get('name'))
+        print(commit.get('sha'), end=": ")
+        print(commit.get('commit').get('author').get('name'))
