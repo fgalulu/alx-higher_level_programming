a712a18e7900f9c4ea5741bbbdb165ec3a20d285
fgalulu <fionasonny132@gmail.com>
2022-09-24 06:17:25 +0000

    task 0 - edit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script thattakes in a URL, sends a request to that URL and displays
-#the bosy of the response.
-
+# Bash script thattakes in a URL, sends a request to that URL
 curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
