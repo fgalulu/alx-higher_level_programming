1798886896f0c231819ab1497417108a3f96997d
fgalulu <fionasonny132@gmail.com>
2022-09-30 09:54:49 +0000

    advanced tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""
-returns a string “BestSchool” n times the number of the iteration
-"""
+"""returns a string “BestSchool” n times the number of the iteration"""
+
+
 def magic_string():
+    """returns a string “BestSchool” n times the number of the iteration"""
     setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
     return ("Best School, " * getattr(magic_string, "n", 0))[:-2]
